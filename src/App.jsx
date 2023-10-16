a5d196cb9bf1e4fbbb2fed9d316db97e5922fb9a
Asle Sayed <sayedasaimaa786@gmail.com>
2023-10-16 09:54:06 +0200

    props.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,26 @@ import './App.css'
 import MyTitle from "./components/MyTitle"
 import PokemonCard from "./components/PokemonCard"
 
+
+const pokemonList = [
+  {
+    name:"bulbasaur",
+    imgSrc:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    
+
+  },
+];
+
 function App() {
   return (
-    <div>
-      <PokemonCard/>
-      <img className="avatar" />
-      
-    </div>
-  );
+    <PokemonCard pokemon={pokemonList[1]} />
+    );
 }
+
 
 export default App;
 
